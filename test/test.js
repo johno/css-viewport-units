7e8f58f62a50dbc1630f645d9304ed28a53dcbb8
John Otander <johnotander@gmail.com>
2015-01-21 11:47:23 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var cssViewportUnits = require('..');
+
+describe('css-viewport-units', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(cssViewportUnits(), true);
+  });
+});
